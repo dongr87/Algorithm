ee5d150a4c2c77673bb94f7de43988627a387d06
Dong Wang <mtwcore@gmail.com>
2016-02-16 13:52:12 -0500

    test

---
+++ queue.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(item):
+        self.items.insert(0, item)
+
+    def dequeue():
+        self.items.pop()
+
+    def isEmpty():
+        return self.items == []
+
+    def size():
+        return len(self.items)
